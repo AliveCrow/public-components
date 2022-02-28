1efb81dff8715a6ec70b73bf548a214aa10c1e79
grit <136907108@qq.com>
2022-02-28 22:45:26 +0800

    Initial storybook-static subtree commit

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Description: 输入文件描述信息
- * @Author: liu-wb
- * @Date: 2022-02-21 09:33:12
- * @LastEditTime: 2022-02-21 09:33:13
- */
-module.exports = {
-  extends: ['@commitlint/config-conventional']
-};
